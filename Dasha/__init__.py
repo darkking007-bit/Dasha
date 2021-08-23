9c4fef724b383f6d8d5fe7ce3d92b7d5e5edad90
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-08-23 12:24:55 +0530

    Update __init__.py

--- Dasha/__init__.py
+++ Dasha/__init__.py
@@ -1,14 +1,29 @@
 #Lol 
-from telethon import TelegramClient
-from decouple import config
 import logging
+import os
+import sys
 import time
+import os
+import urllib.parse as urlparse
+import json
+from logging import basicConfig
+from logging import DEBUG
+from logging import getLogger
+from logging import INFO
 
+
+from telethon import TelegramClient
+StartTime = time.time()
+
+#aah lol
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+ENV = bool(os.environ.get("ENV", True))
 
-APP_ID = config("APP_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
+if ENV:
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+    API_KEY = os.environ.get("API_KEY", None)
+    API_HASH = os.environ.get("API_HASH", None)
 
 kid = TelegramClient('kid', APP_ID, API_HASH).start(bot_token=BOT_TOKEN)
