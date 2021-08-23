fd6a4d5baf971b027fa20a0a28b6f6abfe6fe981
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-08-23 12:31:19 +0530

    Update __main__.py

--- Dasha/__main__.py
+++ Dasha/__main__.py
@@ -1,1 +1,16 @@
+rom sys import argv, exit
+from Dasha import kid
+from Dasha import BOT_TOKEN
 
+import Dasha.events
+
+try:
+    kid.start(bot_token=BOT_TOKEN)
+except Exception:
+    print("Bot Token Invalid")
+    exit(1)
+
+if len(argv) not in (1, 3, 4):
+    kid.disconnect()
+else:
+    kid.run_until_disconnected()
