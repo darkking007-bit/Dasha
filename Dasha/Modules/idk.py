562bed12ddbcbb39a691858ba75b58c43344886b
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-08-23 13:09:27 +0530

    Update idk.py

--- Dasha/Modules/idk.py
+++ Dasha/Modules/idk.py
@@ -2,6 +2,6 @@ from telethon import events, buttons
 from Dasha import kid
 
 
-@kid.on(events.NewMessage(pattern = '[*?/]lol'))
+@kid.on(events.NewMessage(pattern = "[/]lol"))
 async def lol(event):
     await event.reply('lmao')
