4548a326a3525f2ab15314b5f2da4e75f0507b0a
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-11 00:29:35 +0530

    Create __init__.py

---
+++ Dasha/modules/__init__.py
@@ -0,0 +1,54 @@
+import time
+from Dasha import ubot
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+
+async def get_user(event):
+    args = event.pattern_match.group(1).split(" ", 1)
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_obj = await ubot.get_entity(previous_message.sender_id)
+        extra = event.pattern_match.group(1)
+    elif args:
+        extra = None
+        user = args[0]
+        if len(args) == 2:
+            extra = args[1]
+        if user.isnumeric():
+            user = int(user)
+        if not user:
+            await event.edit("I don't know who you're talking about, you're going to need to specify a user...!")
+            return
+        try:
+            user_obj = await ubot.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return
+
+    return user_obj, extra
