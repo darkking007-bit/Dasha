54de7418fd9c034dba49b3c56ec1d6c9c4de962c
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-11 00:28:08 +0530

    Create admin.py

---
+++ Dasha/modules/admin.py
@@ -0,0 +1,21 @@
+from Dasha import ubot, tbot
+from Dasha.events import Wbot
+from . import get_user
+
+@dasha(pattern="^/kick ?(.*)")
+async def kick(event):
+ if event.is_private:
+   return await event.edit("Please use this in groups.")
+ try:
+  user, extra = await get_user(event)
+ except TypeError:
+  pass
+ if not user:
+  await event.edit("Failed to fetch user.")
+ if not event.chat.admin_rights.ban_users:
+   return await event.edit("Failed to Kick, No CanBanUsers Right.")
+ try:
+  await ubot.kick_participant(event.chat_id, user.id)
+  await event.edit(f"Kicked **{user.first_name}** from {event.chat.title}!")
+ except:
+  await event.edit("Can't kick admins.")
