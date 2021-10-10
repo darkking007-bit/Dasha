a10ff1c7a0fd036c260a97bcebf5a0b3f744c2f5
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-10 23:31:27 +0530

    Update __init__.py

--- Dasha/__init__.py
+++ Dasha/__init__.py
@@ -1,29 +1,25 @@
-#Lol 
-import logging
-import os
-import sys
-import time
-import os
-import urllib.parse as urlparse
-import json
-from logging import basicConfig
-from logging import DEBUG
-from logging import getLogger
-from logging import INFO
-
-
 from telethon import TelegramClient
+from telethon.sessions import StringSession
+import time
+import os, logging
+from logging import basicConfig, getLogger, INFO
+"""Dasha"""
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+LOGGER = logging.getLogger(__name__)
+
 StartTime = time.time()
 
-#aah lol
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
-LOGGER = logging.getLogger(__name__)
-ENV = bool(os.environ.get("ENV", True))
+STRING_SESSION = os.environ.get("STRING_SESSION")
+D_SESSION = os.environ.get("D_SESSION")
+X_SESSION = os.environ.get("X_SESSION")
+TOKEN = os.environ.get("TOKEN")
+OWNER_ID = os.environ.get("OWNER_ID")
+API_KEY = os.environ.get("API_KEY")
+API_HASH = os.environ.get("API_HASH")
 
-if ENV:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    API_ID = os.environ.get("API_ID", None)
-    API_HASH = os.environ.get("API_HASH", None)
-
-kid = TelegramClient('kid', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+ubot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
+tbot = TelegramClient(None, API_KEY, API_HASH)
+dbot = TelegramClient(StringSession(D_SESSION), API_KEY, API_HASH)
+xbot = TelegramClient(StringSession(X_SESSION), API_KEY, API_HASH)
