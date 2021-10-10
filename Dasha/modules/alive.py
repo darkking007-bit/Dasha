ffff8823f7f3f597c203dee3c6a632aae2f7e7d9
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-11 00:42:08 +0530

    Create alive.py

---
+++ Dasha/modules/alive.py
@@ -0,0 +1,24 @@
+from Dasha.events import Wbot
+from Dasha import StartTime
+import time, datetime
+from . import get_readable_time
+
+@dasha(pattern="^/ping")
+async def _(event):
+    start_time = datetime.datetime.now()
+    message = await event.edit("`Pinging..`")
+    end_time = datetime.datetime.now()
+    pingtime = end_time - start_time
+    telegram_ping = str(round(pingtime.total_seconds(), 2)) + "s"
+    uptime = get_readable_time((time.time() - StartTime))
+    await message.edit(
+        "<b>Pong !! </b> <code>{}</code>\n"
+        "<b>Uptime -</b> <code>{}</code>".format(telegram_ping, uptime),
+        parse_mode="html",
+    )
+
+@dasha(pattern="^/alive$")
+async def alive(event):
+ first_name = event.sender.first_name
+ last_name = event.sender.last_name
+ user_id = event.sender_id
