4dfd211cfb72591ce59cd19e930c5e77d6cfb948
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-10 23:34:07 +0530

    Update __main__.py

--- Dasha/__main__.py
+++ Dasha/__main__.py
@@ -1,16 +1,17 @@
-from sys import argv, exit
-from Dasha import kid
-from Dasha import BOT_TOKEN
-
+from Dasha import ubot, tbot, dbot, xbot, TOKEN
 import Dasha.events
+import sys
 
 try:
-    kid.start(bot_token=BOT_TOKEN)
-except Exception:
-    print("Bot Token Invalid")
-    exit(1)
-
-if len(argv) not in (1, 3, 4):
-    kid.disconnect()
-else:
-    kid.run_until_disconnected()
+  dbot.start()
+  xbot.start()
+  ubot.start()
+  tbot.start(bot_token=TOKEN)
+except:
+  print("Invalid STRING/Token__bot exiting...")
+  sys.exit()
+  
+ubot.run_until_disconnected()
+xbot.run_until_disconnected()
+dbot.run_until_disconnected()
+tbot.run_until_disconnected()
